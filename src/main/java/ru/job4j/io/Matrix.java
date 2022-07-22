f69234bb17256abef2a5508a07f87a1c1af88969
dim0nster <32572277+dim0nster@users.noreply.github.com>
2022-07-23 01:44:03 +0300

    0.1. FileOutputStream. [#252488 #324985]

--- src/main/java/ru/job4j/io/Matrix.java
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Matrix {
     public static void multiple(int size) {
@@ -13,7 +14,7 @@ public class Matrix {
                 }
                 out.write(System.lineSeparator().getBytes());
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
                 e.printStackTrace();
         }
     }
