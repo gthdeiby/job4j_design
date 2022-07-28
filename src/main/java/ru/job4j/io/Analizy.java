6e48a300299bdb34580e496c32f358dba1306c23
dim0nster <32572277+dim0nster@users.noreply.github.com>
2022-07-28 23:46:42 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analizy {
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            boolean availability = true;
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                String[] src = line.split(" ");
+                if (availability && (src[0].equals("400") || src[0].equals("500"))) {
+                    out.write(src[1] + ";");
+                    availability = false;
+                } else if (!availability && ("200".equals(src[0]) || "300".equals(src[0]))) {
+                    out.println(src[1] + ";");
+                    availability = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analizy analizy = new Analizy();
+        analizy.unavailable("./data/server.log", "./data/unavailable.csv");
+    }
+}
