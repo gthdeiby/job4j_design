10c5516430f76acdc1bf737f3e96d7144e81dfbc
dim0nster <32572277+dim0nster@users.noreply.github.com>
2022-08-06 13:35:30 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,29 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validateArgs(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validateArgs(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Argument pattern violation");
+        }
+        if (!Paths.get(args[0]).toFile().exists()) {
+            throw new IllegalArgumentException(String.format("%s is not exist", args[0]));
+        }
+        if (!Paths.get(args[0]).toFile().isDirectory()) {
+            throw new IllegalArgumentException(String.format("%s is not directory", args[0]));
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException(String.format("%s is not file extension", args[1]));
+        }
     }
 }
