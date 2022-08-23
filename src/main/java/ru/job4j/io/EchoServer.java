7e030cf388da752ddf7582f4226af85fc72b3471
dim0nster <32572277+dim0nster@users.noreply.github.com>
2022-08-23 10:53:29 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -12,9 +12,20 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
+                    String start = in.readLine();
+                    System.out.println(start);
+                    String message = start.split(" ")[1].split("=")[1];
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    if (in.readLine().contains("/?msg=Bye")) {
-                        server.close();
+                    switch (message) {
+                        case "Hello":
+                            out.write("Hello!".getBytes());
+                            break;
+                        case "Exit":
+                            out.write("Bye!".getBytes());
+                            server.close();
+                            break;
+                        default:
+                            out.write("What?".getBytes());
                     }
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
