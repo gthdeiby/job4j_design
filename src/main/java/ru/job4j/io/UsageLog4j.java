0006d1cd9aef27e174d7c8ac79d2d29f03f3695d
dim0nster <32572277+dim0nster@users.noreply.github.com>
2022-08-25 21:57:30 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,14 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        boolean condition = true;
+        char letter = 'O';
+        byte number = 1;
+        short position = 10;
+        int age = 29;
+        long capacity = 100500;
+        float length = 10.5f;
+        double height = 1.88;
+        LOG.debug("condition : {},\nletter : {},\nnumber : {},\nposition : {},\nage : {},\ncapacity : {},\nlength : {},\nheight : {},\n", condition, letter, number, position, age, capacity, length, height);
     }
 }
